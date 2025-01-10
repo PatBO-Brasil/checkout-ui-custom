f491c6fad7ed72cd4b32fa23e8c73b28628b2d08
Mario Sergio <mario@inovaki.com.br>
2025-01-10 02:28:58 -0300

    clarity pixel installation

--- checkout-ui-custom/checkout6-custom.js
+++ checkout-ui-custom/checkout6-custom.js
@@ -10,18 +10,23 @@
     sendGA4Events
   }
 
-    if (!gtmId) {
+  if (!gtmId) {
     console.error('Warning: No Google Tag Manager ID is defined. Please configure it in the apps admin.');
   } else {
     window.dataLayer = window.dataLayer || [];
     window.dataLayer.push({ 'gtm.blacklist': [] });
-  // GTM script snippet. Taken from: https://developers.google.com/tag-manager/quickstart
-  (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-                                                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    // GTM script snippet. Taken from: https://developers.google.com/tag-manager/quickstart
+    (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+      new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
       j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-        'https://inc.patbo.com.br/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-                      })(window,document,'script','dataLayer',gtmId)
+      'https://inc.patbo.com.br/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer',gtmId)
   }
+  (function(c,l,a,r,i,t,y){
+    c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+    t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+    y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+  })(window, document, "clarity", "script", "pq803arm05");
 })()
 /** GTM TAG **/
 
